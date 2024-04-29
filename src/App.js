a52f5d85684db064e84fd3da3e85e2758db995dc
sivanaidupattapu <adisivanaidup@gmail.com>
2024-04-29 12:11:56 +0530

    hl

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import Todos from './components/to-do-li';
-import Counter from './components/counter';
-
+import { useState } from 'react';
 function App() {
+  var [x, setX] = useState(0)
+  function inc(){
+   return setX(()=> x+1)
+  }
+  function dec(){
+    return setX(()=> x-1)
+  }
   return (
-    <div className="App">
-     <Todos></Todos>
-     <Counter></Counter>
-    </div>
-  );
-}
+    <div className="border border-2 border-danger p-2">
+      {
+        <h2>Counter {x}</h2>
+      }
+      {
+        <button onClick={()=>{inc()}}>Increment</button>
+      }
+      {
+        <button onClick={()=>{dec()}}>Decrement</button>
 
+      }
+    </div>
+  )
+}
 export default App;
